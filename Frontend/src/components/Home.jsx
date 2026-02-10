31ec0ef36f1742f8b5e6f5a840dd07b7516f3ee0
Bhuvan <bhuvanasairam2005@gmail.com>
2026-02-10 10:33:20 +0530

    removed unwanted console statement

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -14,7 +14,6 @@ function Home() {
     const fetchHomeData = async () => {
       try {
         const res = await axios.get("http://localhost:5000/api/auth/homeData");
-         console.log(res.data);
         if (res.data.success) {
           setStats({
             totalProjects: res.data.data.totalProjects,
